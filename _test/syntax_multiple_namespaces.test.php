2594ac02d900ffa010a57d6edbf5a078930006dd
Martin Rieß <martin.riess@p-riess.at>
2026-09-08 19:30:44 +0200

    Add test for multiple namespace filters

---
+++ _test/syntax_multiple_namespaces.test.php
@@ -0,0 +1,127 @@
+<?php
+
+/*
+ * Copyright (c) 2016 Mark C. Prins <mprins@users.sf.net>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE,
+ * DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+ * OF THIS SOFTWARE.
+ */
+
+use dokuwiki\Logger;
+use dokuwiki\Search\Indexer;
+use DOMWrap\Document;
+
+/**
+ * Syntax tests for multiple namespace filters.
+ *
+ * @group plugin_backlinks
+ * @group plugins
+ */
+class syntax_multiple_namespaces_plugin_backlinks_test extends DokuWikiTest
+{
+    protected $pluginsEnabled = array('backlinks');
+
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+
+        global $conf;
+
+        $conf['allowdebug'] = 1;
+
+        TestUtils::rcopy(TMP_DIR, dirname(__FILE__) . '/data/');
+        Logger::debug("set up class syntax_multiple_namespaces_plugin_backlinks_test");
+    }
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        global $conf;
+
+        $conf['allowdebug'] = 1;
+        $conf['cachetime'] = -1;
+
+        $verbose = false;
+        $force = false;
+
+        $data = array();
+        search($data, $conf['datadir'], 'search_allpages', array('skipacl' => true));
+
+        foreach ($data as $val) {
+            (new Indexer())->addPage($val['id'], $verbose, $force);
+        }
+
+        if ($conf['allowdebug']) {
+            touch(DOKU_TMP_DATA . 'cache/debug.log');
+        }
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+
+        global $conf;
+
+        if ($conf['allowdebug']) {
+            print "\n";
+            readfile(DOKU_TMP_DATA . 'cache/debug.log');
+            unlink(DOKU_TMP_DATA . 'cache/debug.log');
+        }
+    }
+
+    public function testMultipleNamespaces(): void
+    {
+        $request = new TestRequest();
+
+        $response = $request->get(array('id' => 'mmm:nnn:ooo:multiple_namespaces'));
+
+        $this->assertTrue(
+            str_contains($response->getContent(), 'linking to a page form aaa'),
+            '"linking to a page form aaa" should be in the output'
+        );
+
+        $this->assertFalse(
+            str_contains($response->getContent(), 'linking to a page b'),
+            '"linking to a page b" should not be in the output'
+        );
+
+        $this->assertFalse(
+            str_contains($response->getContent(), 'linking to a page c'),
+            '"linking to a page c" should not be in the output'
+        );
+
+        $this->assertFalse(
+            str_contains($response->getContent(), 'linking to a page d'),
+            '"linking to a page d" should not be in the output'
+        );
+
+        $doc = (new Document())->loadHTML($response->getContent());
+
+        $this->assertEquals(
+            1,
+            pq('#plugin__backlinks', $doc)->length,
+            'There should be one backlinks element'
+        );
+
+        $wikilinks = pq('#plugin__backlinks ul li', $doc);
+
+        Logger::debug('found backlinks', $wikilinks->text());
+
+        $this->assertEquals(
+            1,
+            $wikilinks->contents()->length,
+            'There should be 1 backlink'
+        );
+    }
+}
